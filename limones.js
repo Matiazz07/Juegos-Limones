6d8cad0c940bde29c8dce7f73f296e13aa02a511
Matiazz07 <damian.villota.2007y@gmail.com>
2026-04-12 16:19:45 -0500

    agregar mover limon

--- limones.js
+++ limones.js
@@ -4,8 +4,12 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=40;
 const ALTURA_PERSONAJE=75;
 const ANCHO_PERSONAJE=50;
+const ALTURA_LIMON=15;
+const ANCHO_LIMON=15;
 
 let perosnajeX=canvas.width/2;
+let limonX=0;
+let limonY=0;
 
 function dibujarSuelo(){
     ctx.fillStyle="green";
@@ -20,6 +24,7 @@ function dibujarPersonaje(){
 function dibujar(){
     dibujarPersonaje();
     dibujarSuelo();
+    dibuajrLimon();
 }
 
 function moverIzquierda(){
@@ -36,8 +41,19 @@ function actualizarPantalla(){
     limpiarCanvas();
     dibujarPersonaje();
     dibujarSuelo();
+    dibuajrLimon();
 }
 
 function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function dibuajrLimon(){
+    ctx.fillStyle="yellow";
+    ctx.fillRect(limonX,limonY,ALTURA_LIMON,ANCHO_LIMON);
+}
+
+function moverLimon(){
+    limonY=limonY+10;
+    actualizarPantalla();
 }
