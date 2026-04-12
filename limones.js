d268d25d95de4086fdbd89ffd2bea1df6436443b
Matiazz07 <damian.villota.2007y@gmail.com>
2026-04-12 16:02:59 -0500

    agregar mover izquierda y derecha

--- limones.js
+++ limones.js
@@ -5,6 +5,8 @@ const ALTURA_SUELO=40;
 const ALTURA_PERSONAJE=75;
 const ANCHO_PERSONAJE=50;
 
+let perosnajeX=canvas.width/2;
+
 function dibujarSuelo(){
     ctx.fillStyle="green";
     ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
@@ -12,11 +14,30 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="red";
-    ctx.fillRect(canvas.width/2-(ANCHO_PERSONAJE/2),canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(perosnajeX-(ANCHO_PERSONAJE/2),canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 }
-
 
 function dibujar(){
     dibujarPersonaje();
     dibujarSuelo();
+}
+
+function moverIzquierda(){
+    perosnajeX=perosnajeX-10;
+    actualizarPantalla();
+}
+
+function moverDerecha(){
+    perosnajeX=perosnajeX+10;
+    actualizarPantalla();
+}
+
+function actualizarPantalla(){
+    limpiarCanvas();
+    dibujarPersonaje();
+    dibujarSuelo();
+}
+
+function limpiarCanvas(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
