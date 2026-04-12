f94d6538ae25af58d395e47440aa658d0f66dd2b
Matiazz07 <damian.villota.2007y@gmail.com>
2026-04-12 15:45:38 -0500

    agregar Elemnetos, suelo y personaje

--- limones.js
+++ limones.js
@@ -1,2 +1,22 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
+
+const ALTURA_SUELO=40;
+const ALTURA_PERSONAJE=75;
+const ANCHO_PERSONAJE=50;
+
+function dibujarSuelo(){
+    ctx.fillStyle="green";
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
+}
+
+function dibujarPersonaje(){
+    ctx.fillStyle="red";
+    ctx.fillRect(canvas.width/2-(ANCHO_PERSONAJE/2),canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+
+
+function dibujar(){
+    dibujarPersonaje();
+    dibujarSuelo();
+}
